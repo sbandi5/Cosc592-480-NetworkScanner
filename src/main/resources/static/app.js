95eea659a66782756c160f26a57251667c55e9e1
sbandi5 <bandisaimani@gmail.com>
2025-03-17 10:21:08 -0400

    successfully added some more methods for nmap and basic webpage look of frontend

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,12 @@
 async function scanPorts() {
     let target = document.getElementById("scanTarget").value;
-    let response = await fetch(`/api/scan-sniff?target=${target}`);
+    let scanOption = document.getElementById("selection").value;
+    let response = null;
+    if(scanOption === 'ip'){
+        response = await fetch(`/api/scan?target=${target}`);
+    }else if(scanOption === 'port'){
+        response = await fetch(`/api/scan-sniff?target=${target}`);
+    }
     let result = await response.text();
     document.getElementById("scanResult").innerText = result;
 }
@@ -16,4 +22,21 @@ async function runExploit() {
     let response = await fetch(`/api/exploit?target=${target}`, { method: 'POST' });
     let result = await response.text();
     document.getElementById("exploitResult").innerText = result;
+}
+
+async function unhide() {
+    let selection = document.getElementById("selection").value;
+    let searchBox = document.getElementById("searchBox");
+
+    if (selection) {
+        searchBox.classList.remove("hidden");
+    } else {
+        searchBox.classList.add("hidden");
+    }
+}
+
+
+
+async function showSearchBox(tool) {
+    alert("You selected: " + tool);
 }
